e489b7f2186b6160e2155a4d4336765f7965da2c
Mal <malrsteffes@gmail.com>
2020-10-01 22:36:07 -0400

    toggle works

---
+++ app.js
@@ -0,0 +1,9 @@
+let toggle = document.getElementById("toggle");
+let body = document.getElementsByTagName("body");
+
+function toggleClass() {
+  console.log("clicked!");
+  if (document.getElementById("bodyToggle").className == "dark")
+    document.getElementById("bodyToggle").className = "light";
+  else document.getElementById("bodyToggle").className = "dark";
+}
